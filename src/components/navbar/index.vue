f816a78c203666fc3c6ce23fb59f79f167344476
zhang <iyunx@qq.com>
2023-06-09 18:01:08 +0800

    fix: company

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -3,7 +3,13 @@
     <div class="left-side">
       <a-space>
         <img alt="logo" src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image" />
-        <a-typography-title :style="{ margin: 0, fontSize: '18px' }" :heading="5"> Arco Pro - {{ appStore.device }} </a-typography-title>
+        <a-typography-title v-show="appStore.device === 'desktop'" :style="{ margin: 0, fontSize: '18px' }" :heading="5"> Arco Pro </a-typography-title>
+        <a-select default-value="华阳">
+          <a-option>华阳</a-option>
+          <a-option>新红</a-option>
+          <a-option>新福</a-option>
+          <a-option>家福</a-option>
+        </a-select>
         <icon-menu-fold v-if="!topMenu && appStore.device === 'mobile'" style="font-size: 22px; cursor: pointer" @click="toggleDrawerMenu" />
       </a-space>
     </div>
